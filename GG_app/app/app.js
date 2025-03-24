f38015fa48b9841ba0a4df7bfd03e5975351b6ac
ArtemShkurat <artem.shkurat04@gmail.com>
2025-03-24 13:55:45 +0200

    Deleted useless functions and started coding.

--- GG_app/app/app.js
+++ GG_app/app/app.js
@@ -13,65 +13,46 @@ app.set('views', './app/views');
 
 // Get the functions in the db.js file to use
 const db = require('./services/db');
+app.use(express.urlencoded({ extended: true }));
 
-// Get the student model
-const { Student } = require("./classes/student");
+// Get the models
+const { User } = require("./classes/user");
+
+// Set the sessions
+var session = require('express-session');
+app.use(session({
+  secret: 'secretkeysdfjsflyoifasd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.render("index");
+    console.log(req.session);
+    if (req.session.uid) {
+		res.send('Welcome back, ' + req.session.uid + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
 });
 
-// JSON output of all programmes
-app.get("/all-programmes", function(req, res) {
-    var sql = 'select * from Programmes';
-    // As we are not inside an async function we cannot use await
-    // So we use .then syntax to ensure that we wait until the 
-    // promise returned by the async function is resolved before we proceed
-    db.query(sql).then(results => {
-        console.log(results);
-        res.json(results);
-    });
+// Register
+app.get('/register', function (req, res) {
+    res.render('register');
 });
 
-// Single programme page (no formatting or template)
-app.get("/programme-single/:id", async function (req, res) {
-    var pCode = req.params.id;
-    var pSql = "SELECT * FROM Programmes WHERE id = ?";
-    var results = await db.query(pSql, [pCode]);
-    //Now call the database for the modules
-    //Why do you think that the word modules is coming in before the name of the programme??
-    var modSql = "SELECT * FROM Programme_Modules pm \
-    JOIN Modules m on m.code = pm.module \
-    WHERE programme = ?";
-    var modResults = await db.query(modSql, [pCode]);
-    // String the results together, just for now.  Later we will push this
-    // through the template
-    res.send(JSON.stringify(results) + JSON.stringify(modResults));  
+// Login
+app.get('/login', function (req, res) {
+    res.render('login');
 });
 
-
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    var sql = 'select * from test_table';
-    // As we are not inside an async function we cannot use await
-    // So we use .then syntax to ensure that we wait until the 
-    // promise returned by the async function is resolved before we proceed
-    db.query(sql).then(results => {
-        console.log(results);
-        res.json(results)
-    });
-});
-
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
-
-app.get("/hello/:name", function(req, res) {
-    console.log(req.params);
-    res.send("Hello " + req.params.name);
-});
+// Logout
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login');
+  });
 
 // Start server on port 3000
 app.listen(3000,function(){
---
+++ GG_app/app/controllers/authenticate.js
@@ -0,0 +1,26 @@
+// Check submitted email and password pair
+app.post('/authenticate', async function (req, res) {
+    params = req.body;
+    var user = new User(params.email);
+    try {
+        uId = await user.getIdFromEmail();
+        if (uId) {
+            match = await user.authenticate(params.password);
+            if (match) {
+                req.session.uid = uId;
+                req.session.loggedIn = true;
+                console.log(req.session.id);
+                res.redirect('/single-student/' + uId);
+            }
+            else {
+                // TODO improve the user journey here
+                res.send('invalid password');
+            }
+        }
+        else {
+            res.send('invalid email');
+        }
+    } catch (err) {
+        console.error(`Error while comparing `, err.message);
+    }
+});
